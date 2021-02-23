8ba566493609cdd6b19f24e387b605510b129287
Gabriel <gabrielnabas@id.uff.br>
2021-02-23 18:26:18 -0300

    .

--- lib/constants.ts
+++ lib/constants.ts
@@ -21,9 +21,9 @@ export const BRAND_NAME = 'ACME';
 export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
 export const SITE_NAME = 'ACME Conf';
 export const META_DESCRIPTION =
-  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
+  'Tire o valor oculto em seus dados de forma interativa, simples e fácil.';
 export const SITE_DESCRIPTION =
-  'An interactive online experience by the community, free for everyone.';
+  'Tire o valor oculto em seus dados de forma interativa, simples e fácil.';
 export const DATE = 'October 27, 2020';
 export const SHORT_DATE = 'Oct 27 - 9:00am PST';
 export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
